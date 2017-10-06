a7b1f2afabc67acba7287dc30d14a3fed0f3bf75
Paul Elion <paul.j.elion@gmail.com>
2017-10-06 11:55:15 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var d, h, m, s, color;
+function displayTime() {
+	d = new Date();
+	h = d.getHours();
+	m = d.getMinutes();
+	s = d.getSeconds();
+	
+	
+	if(h <= 9) h = '0'+h;
+	if(m <= 9) m = '0'+m;
+	if(s <= 9) s = '0'+s;
+	
+	color = "#"+h+m+s;
+	
+	document.body.style.background = color;
+	
+	document.getElementById("clock").innerHTML = `${h-12}:${m}:${s}`;
+	
+	setTimeout(displayTime, 1000);
+}
+
+
+displayTime();
